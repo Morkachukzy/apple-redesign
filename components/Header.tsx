c70c472c7068678f1ebfca6bf3a14a01e9b99138
Chukwuka Morka <61619525+Morkachukzy@users.noreply.github.com>
2022-09-04 08:24:57 +0100

    Initialize Create Next App with TailwindCSS

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  SearchIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {};
+
+export default Header;
