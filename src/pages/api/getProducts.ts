9a36c9b679362667f9b4e4e6f36d0f89497ff1aa
Chukwuka Morka <61619525+Morkachukzy@users.noreply.github.com>
2022-09-05 09:20:01 +0100

    add categories and product helpers

---
+++ src/pages/api/getProducts.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { sanityClient } from "../../../sanity";
+
+const query = groq`*[_type == "product"] {
+  _id,
+  ...
+}`;
+type Data = {
+  products: [Product];
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const products = await sanityClient.fetch(query);
+  res.status(200).json({ products });
+};
+export default handler;
